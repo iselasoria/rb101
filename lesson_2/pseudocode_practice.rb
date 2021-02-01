a538a47a0981bbfd9c2de71e6e76626d907af6fb
Rosa <rosaiselasoriamonzon@gmail.com>
2021-01-31 20:28:00 -0800

    adding practice

---
+++ lesson_2/pseudocode_practice.rb
@@ -0,0 +1,19 @@
+# write code that has:
+# a method that returns the sum of two integers
+# a method that takes an array of strings, and returns a string that is all those strings concatenated together
+# a method that takes an array of integers, and returns a new array with every other element
+
+define method 
+  sum = number1 + number 2
+returns sum 
+
+define method that takes an array
+  iterate through the array
+    print each item in the array in the same line through concatenation
+
+
+define a method that takes an array of integers
+iterate through array
+next on every other item 
+push every other itme (not skipped) into a new array 
+return the new array 
