20a1136f62e671c4d934edc43f656f24f9dc06f2
Rosa <rosaiselasoriamonzon@gmail.com>
2021-02-11 20:38:02 -0800

    adding new features to calc

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -1,6 +1,8 @@
 #### assignment: mortgage calculator #####
 
-
+def prompt(message)
+  puts "=> #{message}"
+end 
 
 def validamount?(amount)
   amount.to_i.to_s == amount && amount.to_i > 20000
@@ -14,64 +16,77 @@ def validrate?(myrate)
   myrate.to_i.to_s == myrate || myrate.to_f.to_s == myrate
 end
 
+def apr_to_pct(apruser)
+  apruser.to_f / 100.0
+end
+
+#apr_aspct(6.6)
+
+#apr = (apr_raw.to_i / 100.0) 
+
+ # monthly_rate = apr.to_f / 12
+
+
+
 loop do 
   borrowed = ''
   loop do 
-    puts "How much money do you need to borrow?"
+    prompt("How much money do you need to borrow?")
     borrowed = gets().chomp()
 
 
     if validamount?(borrowed)
       break
     else
-      puts "Please enter a valid amount: "
+      prompt("Please enter a valid amount: ")
     end
   end 
 
   years=''
   loop do 
-    puts "How many years do you want your loan to be?"
+    prompt("How many years do you want your loan to be?")
     years = gets().chomp()
     if validtimeframe?(years)
       break
     else 
-      puts "Your timeframe should be more than 7 years and fewer than 30."
+      prompt("Your timeframe should be more than 7 years and fewer than 30.")
     end 
   end 
 
   apr_raw = ''
   loop do
-    puts puts "Enter your APR in the integer format or decimals in the thenths place"
+    prompt("Enter your APR in the integer format or decimals in the thenths place")
     apr_raw = gets().chomp()
     if validrate?(apr_raw)
       break
     else
-      puts "Please enter a valid APR: "
+      prompt("Please enter a valid APR: ")
     end
   end 
 
+  prompt("The percentage you set is #{(apr_to_pct(apr_raw) * 100).round(2)}%")
   months = years.to_i * 12
   puts "Your loan will be for #{months} months. Hang tight, we're calculating your monthly rate."
   sleep(7)
 
   # take user apr and make it a percentage
-  apr = (apr_raw.to_i / 100.0) 
+  apr = (apr_raw.to_f / 100.0) 
 
   monthly_rate = apr.to_f / 12
 
-  puts "Your monthly rate is: #{monthly_rate}. Calculating your monthly mortgage."
+  prompt("Your monthly rate is: #{(monthly_rate).round(3)}. Calculating your monthly mortgage.")
   sleep(7)
   # we can round this up to the hundreds place?
   monthly_payment = borrowed.to_i * (monthly_rate.to_f / (1 - (1 + monthly_rate).to_f**(-months)))
 
-  puts "You can expect to pay $#{monthly_payment.round(2)} per month."
+  prompt("You can expect to pay $#{monthly_payment.round(2)} per month.")
   sleep(5)
 
-  puts "Do you wish to run another scenario?"
+  prompt("Do you wish to run another scenario?")
   another = gets().chomp()
   break unless another.downcase().start_with?('y')
 end 
 
-puts "Thanks for using the mortgage calculator, come back again anytime!"
+prompt("Thanks for using the mortgage calculator, come back again anytime!")
 
 
