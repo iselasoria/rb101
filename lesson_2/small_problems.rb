b9ac2b50ef3165b21b6e3721c47c3fbb97c01343
Rosa <rosaiselasoriamonzon@gmail.com>
2021-02-14 19:15:59 -0800

    adding two more questions

--- lesson_2/small_problems.rb
+++ lesson_2/small_problems.rb
@@ -35,9 +35,52 @@ puts is_odd(7)
 #Q3 List of digits. Write a method that takes one argument,
 # a positive integer, and returns a list of the digits in the number.
 
-def digit_list(mydigit)
-  myarray = []
-  myarray << mydigit.split('')
+def digit_list(number)
+  digits = []
+  loop do
+    number, remainder = number.divmod(10)
+    digits.unshift(remainder)
+    break if number == 0
+  end
+  digits
+end
+
+#Q4 Write a method that counts each occurance in the array.
+vehicles = [
+  'car', 'car', 'truck', 'car', 'SUV', 'truck',
+  'motorcycle', 'motorcycle', 'car', 'truck'
+]
+=begin 
+start counters of each ttype
+iterate 
+each time it finds one, add counter for that
+=end
+
+def count_occurrences(vehicles)
+  occurences = {}
+
+  vehicles.uniq.each do |v|
+    occurences[v] = vehicles.count(v)
+  end
+
+  occurences.each do |v, c|
+    puts "#{v} => #{c}"
+  end
+
+end 
+puts count_occurrences(vehicles)
+
+#Q5 Write a method that takes one argument, a string, and returns the words in reverse order
+
+def reverseme(mystr)
+  reversed_sentence=''
+  index = 0
+  while index < mystr.length
+    puts index
+    #eversed_sentence = mystr[index] #+ reversed_sentence
+    index += 1
+  end 
+  return reversed_sentence
 end 
 
-puts digit_list(1234)
+puts reverseme("The grey cat sat by the window.")
