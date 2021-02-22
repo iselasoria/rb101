655652cdcad589b0cdf65dc8abd4078edd95b21c
Rosa <rosaiselasoriamonzon@gmail.com>
2021-02-21 19:22:56 -0800

    adding more questions

--- small_problems/sp_easy3.rb
+++ small_problems/sp_easy3.rb
@@ -1,5 +1,6 @@
 ###### EASY 3 ######
-#Write a program that solicits 6 numbers from user then prints a message
+
+#Q1 Write a program that solicits 6 numbers from user then prints a message
 # that describes whether or not the 6th numbers appears amongst the first 5
 numbers = []
 
@@ -22,5 +23,24 @@ else
   puts "The number #{last_number} does not appear in #{numbers}."
 end
 
+# Q2 write a program that asks for two positive integers and prints the results of the following operations
+# on those two numbers: addition, subtraction, product, quotient, remainder and power.
 
- 
+print ("Enter the first number: ")
+first = gets.chomp.to_i
+print("Enter the second number: ")
+second = gets.chomp.to_i
+
+addition = first + second
+puts "#{first} + #{second} = #{addition}"
+subtraction = first - second
+puts "#{first} + #{second} = #{subtraction}"
+product = first * second
+puts "#{first} + #{second} = #{product}"
+quotient = first / second
+puts "#{first} + #{second} = #{quotient}"
+remainder = first % second
+puts "#{first} + #{second} = #{remainder}"
+power = first ** second
+puts "#{first} + #{second} = #{power}"
+
