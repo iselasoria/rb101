d2a23385224a8f21c2c1e2deca263aafb8ed816c
Rosa <rosaiselasoriamonzon@gmail.com>
2021-03-07 13:11:18 -0800

    adding the third round of practice

---
+++ lesson_3/practice_medium1.rb
@@ -0,0 +1,10 @@
+### MEDIUM 1 ###
+
+#Q1 ASCII Art. Print the sentence: The Flintstones Rock! output 10 times
+
+10.times {|number| puts (" " * number) + "The Flintstones Rock!"}
+
+#Q2 The result of the following statement will be an error: 
+puts "the value of 40 + 2 is " + (40 + 2)
+# why is this and what are two possible ways to fix it?
+
