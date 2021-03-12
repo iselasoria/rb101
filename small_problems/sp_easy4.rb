671cd5a8b0b93fd92afba3053f78fec53edf827b
Rosa <rosaiselasoriamonzon@gmail.com>
2021-03-11 22:14:46 -0800

    adding another question

--- small_problems/sp_easy4.rb
+++ small_problems/sp_easy4.rb
@@ -137,3 +137,30 @@ end
 p string_to_integer('234')
 
 
+#Q8 In the previous exericise you developed a method that convers simple numeric strings to integers.
+# Here you will extend that method to work with signed numbers. The string can have a leading + or -
+# if the first char is a + you method should be positive, negative if it a leading -. No sign defaults to
+# positive. 
+
+DIGITS = {
+  '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
+  '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9
+}
+
+def string_to_integer(strnum)
+  digits = strnum.chars.map {|char| DIGITS[char]}
+
+  value = 0
+  digits.each {|digit| value = 10 * value + digit}
+  value
+end
+
+def string_to_signed_integer(string)
+  case string[0]
+  when '-' then -string_to_integer(string[1..-1])
+  when '+' then string_to_integer(string[1..-1])
+  else          string_to_integer(string)
+  end
+end
+
+p string_to_signed_integer("-3543")
